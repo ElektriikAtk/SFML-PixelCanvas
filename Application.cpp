e5168d75c69a53113b9a7ae3a804ac21a4e51561
ElektriikAtk <songohansmaka@gmail.com>
2023-07-28 00:01:58 +0200

    Work in progress

--- Application.cpp
+++ Application.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <math.h>
 
 #define WINDOW_HEIGHT 720
 #define WINDOW_WIDTH 1280
@@ -33,17 +35,9 @@ int main()
     text.setPosition(1000, 600);
 
     std::vector<sf::RectangleShape> pixelVector;
-    for (int i = 0; i < WINDOW_HEIGHT; i += pixelSize.y) {
-        sf::RectangleShape pixel(pixelSize);
-        if (i != 0) {
-            pixel.setPosition(sf::Vector2f(0, i));
-        }
-        pixel.setFillColor(sf::Color::White);
-        pixel.setOutlineThickness(1.f);
-        pixel.setOutlineColor(sf::Color::Black);
-        pixelVector.push_back(pixel);
-
-        for (int j = 0; j < pixelSize.x * horizontal_px; j += pixelSize.x) {
+    for (int i = 0; i < pixelSize.y * vertical_px; i += pixelSize.y) {
+        for (int j = 0; j <pixelSize.x * horizontal_px; j += pixelSize.x) {
+            sf::RectangleShape pixel(pixelSize);
             pixel.setFillColor(sf::Color::White);
             pixel.setOutlineThickness(1.f);
             pixel.setOutlineColor(sf::Color::Black);
@@ -67,37 +61,74 @@ int main()
         window.draw(button);
         window.draw(text);
 
-        for (int k = 0; k != sizeof(pixelVector) * 4; k++) {
-            window.draw(pixelVector[k]);
+        for (int i = 0; i < pixelVector.size(); i++) {
+            window.draw(pixelVector[i]);
         }
 
         sf::Vector2i localPosition = sf::Mouse::getPosition(window); //Mouse pos
         
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-            //std::cout << "LocalPos: " << localPosition.x << ", " << localPosition.y << "\n";
-            //std::cout << "\n";
-            //std::cout << "Left: " << pixelVector[0].getGlobalBounds().left << std::endl;
-            //std::cout << "Top: " << pixelVector[0].getGlobalBounds().top << std::endl;
-            //std::cout << "Width: " << pixelVector[0].getGlobalBounds().width << std::endl;
-            //std::cout << "Height: " << pixelVector[0].getGlobalBounds().height << std::endl;
 
-            for (int k = 0; k != sizeof(pixelVector) * 4; k++) {
+            std::ofstream outputFile;
+            outputFile.open("OutputFile.txt");
 
-                int left = pixelVector[k].getGlobalBounds().left;
-                int right = pixelVector[k].getGlobalBounds().left + pixelVector[k].getGlobalBounds().width;
-                int top = pixelVector[k].getGlobalBounds().top;
-                int bottom = pixelVector[k].getGlobalBounds().top + pixelVector[0].getGlobalBounds().height;
+            for (int i = 0; i < pixelVector.size(); i++) {
+                if (!sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
 
-                if (localPosition.x > left && localPosition.x < right && localPosition.y > top && localPosition.y < bottom) {
-                    pixelVector[k].setFillColor(sf::Color::Magenta);
-                    window.draw(pixelVector[k]);                }
+                    int left = pixelVector[i].getGlobalBounds().left;
+                    int right = pixelVector[i].getGlobalBounds().left + pixelVector[i].getGlobalBounds().width;
+                    int top = pixelVector[i].getGlobalBounds().top;
+                    int bottom = pixelVector[i].getGlobalBounds().top + pixelVector[0].getGlobalBounds().height;
+
+                    if (localPosition.x > left && localPosition.x < right && localPosition.y > top && localPosition.y < bottom) {
+                        pixelVector[i].setFillColor(sf::Color::Black);
+                        pixelVector[i].setOutlineColor(sf::Color::White);
+                        window.draw(pixelVector[i]);
+                        std::cout << i << "\n";
+                    }
+                }
+
+                if (localPosition.x > button.getGlobalBounds().left && localPosition.x < button.getGlobalBounds().left + button.getGlobalBounds().width && localPosition.y > button.getGlobalBounds().top && localPosition.y < button.getGlobalBounds().top + button.getGlobalBounds().height) {
+                    std::vector<int> bitmapVector;
+                    text.setString("Inside");
+                    for (int i = 0; i < pixelVector.size(); i++) {
+                        sf::Color pixelColor = pixelVector[i].getFillColor();
+                        if (pixelColor == sf::Color::Black) {
+                            bitmapVector.push_back(1);
+                        }
+                        else {
+                            bitmapVector.push_back(0);
+                        }
+                    }
+                    for (int i = 0; i < bitmapVector.size(); i++) {
+                        outputFile << "Bitmap vector: " << bitmapVector[i];
+                        //std::cout << bitmapVector[i];
+                    }
+
+                    int decVal = 0;
+                    for (int i = 7; i >= 0; i--) {
+                        for (int j = 0; j < 8; j++) {
+                            if (bitmapVector[i] == 1) {
+                                std::cout << "j: " << j << "\n";
+                                decVal += std::pow(2, j);
+                            }
+                        }
+                        std::cout << "decval: " << decVal << "\n";
+                    }
+                }
+                else {
+                    text.setString("Outside");
+                }
+                window.draw(text);
+                outputFile.close();
             }
-        } 
+        }
         else if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
             pixelVector[30].setFillColor(sf::Color::Yellow);
             pixelVector[30].setOutlineColor(sf::Color(0, 0, 0));
             window.draw(pixelVector[30]);
         }
+        
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
         {
